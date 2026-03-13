49c81f85b6cb4daa58df063a843c268874de3770
Wanda McCrae <wanda.mccrae@gmail.com>
2026-03-13 12:14:35 -0400

    Prepared backend for deployment

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,14 @@ import os
 
 from routers import nyt_api_handler
 
+is_dev = os.environ.get("ENV", "production") == "development"
+
 app = FastAPI(
     title="Headline Editor Backend API",
     version="1.0.0",
     description="API for the Headline Editor application, providing endpoints to fetch and manipulate news headlines.\n\nCreation Date: 2024 June 1\n\nAuthor: [Wanda McCrae](https://wandamccrae.com), Copyright 2024.",
+    docs_url="/docs" if is_dev else None,
+    redoc_url="/redoc" if is_dev else None,
 )
 app.include_router(nyt_api_handler.router, tags=["NYT_API"])
 
